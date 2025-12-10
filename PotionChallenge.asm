51bc9f789732645d07558803ee34c669602887ef
kiarraking26-arch <kiarraking26@gmail.com>
2025-12-09 23:14:07 -0800

    Update PotionChallenge.asm

--- PotionChallenge.asm
+++ PotionChallenge.asm
@@ -2,33 +2,37 @@
 # Demonstrates brewing, using potions, slow gravity, and combos
 
 # Initialize registers for ingredients/potions
-harddrop $t0, $zero, 0
-harddrop $t1, $zero, 0
-harddrop $t2, $zero, 0
-harddrop $t3, $zero, 0
+WINGARDIUM $t0,$zero,$t0
+WINGARDIUM $t1,$zero,$t1
+WINGARDIUM $t2,$zero,$t2
+WINGARDIUM $t3,$zero,$t3
 
-# Brew multiple potions
-brew
-brew
-brew
+# Brew multiple potions (random boost)
+EXPECTO_PATRONUM $t0
+EXPECTO_PATRONUM $t1
+EXPECTO_PATRONUM $t2
 
 # Use potions to increase power
-use
-use
+GEMINO $t0,$t1
+GEMINO $t2,$t3
 
 # Slow gravity challenge
-slowgravity $t0, $t1, 2
-slowgravity $t2, $t3, 1
+MUFFLIATO $t0,$t0,2
+MUFFLIATO $t2,$t2,1
 
 # Combine potions for maximum effect
-combo 5
+TIME_TURNER $t0,3
+TIME_TURNER $t1,3
 
 # Check current board/register values
-viewboard
+VIEWBOARD:  # Could be a comment, or just simulate effect
+EXPECTO_PATRONUM $t0
+EXPECTO_PATRONUM $t2
 
 # Learn advanced spell
-learn $t0
-learn $t2
+GEMINO $t0,$t0
+GEMINO $t2,$t2
 
 # Final potion use
-use
+EXPECTO_PATRONUM $t0
+EXPECTO_PATRONUM $t2
