0d4fd342d51e53b0b4289f96d82110b359841097
kiarraking26-arch <kiarraking26@gmail.com>
2025-12-09 23:15:19 -0800

    Update HogwartTournament.asm

--- HogwartTournament.asm
+++ HogwartTournament.asm
@@ -1,40 +1,37 @@
 # Hogwarts Tournament Simulation
-# Showcases casting spells, curses, teleportation, and winning conditions
+# Showcase casting spells, curses, teleportation, and winning conditions
 
 # Initialize player powers
-harddrop $t0, $zero, 8
-harddrop $t1, $zero, 6
-harddrop $t2, $zero, 4
-harddrop $t3, $zero, 7
+WINGARDIUM $t0,$zero,$t0   # Power boost Player 1
+WINGARDIUM $t1,$zero,$t1   # Power boost Player 2
+WINGARDIUM $t2,$zero,$t2   # Power boost Player 3
+WINGARDIUM $t3,$zero,$t3   # Power boost Player 4
 
 # Pre-round magic boost
-brew
-brew
-learn $t0
-learn $t1
+EXPECTO_PATRONUM $t0
+EXPECTO_PATRONUM $t1
 
 # Round 1: Cast spells and curse opponents
-cast $t2, 3
-cast $t3, 2
-curse $t1
-curse $t0
+REDUCTO $t2,$t2,$t0
+REDUCTO $t3,$t3,$t1
+PROTEGO $t1,$t1
+PROTEGO $t0,$t0
 
 # Round 2: Teleport if power sufficient
-tp $t2, TournamentRound2
+PORTKEY $t2,TournamentRound2
 
-# Use magic items strategically
-use
-use
+# Use magical items strategically
+GEMINO $t0,$t0
+GEMINO $t1,$t1
 
 # Tournament rounds loop
 TournamentRound2:
-cast $t0, 2
-cast $t1, 1
-downstack
-combo 4
-viewboard
+REDUCTO $t0,$t0,$t1
+REDUCTO $t1,$t1,$t2
+EXPECTO_PATRONUM $t0
+EXPECTO_PATRONUM $t1
 
 # Final scoring
-allclear
-tetris
-topout
+AVADA_KEDAVRA $t2
+SCURGIFY $t3
+TIME_TURNER $t0,2
